ef6a57a1c1101d4ba09a1f6cfed9246d9436783f
Torosyan David <davit.torosyan.0214@gmail.com>
2024-02-10 16:16:50 +0400

    Builder

---
+++ Builder/ConcarateBuilder.cs
@@ -0,0 +1,40 @@
+﻿using Builder.BulderClasses;
+using Builder.Interfaces;
+
+public class ConcreteBuilder : IBuilder
+{
+    private Product _product = new Product();
+    public ConcreteBuilder()
+    {
+        this.Reset();
+    }
+
+    public void Reset()
+    {
+        this._product = new Product();
+    }
+
+    public void BuildPartA()
+    {
+        this._product.Add("PartA1");
+    }
+
+    public void BuildPartB()
+    {
+        this._product.Add("PartB1");
+    }
+
+    public void BuildPartC()
+    {
+        this._product.Add("PartC1");
+    }
+
+    public Product GetProduct()
+    {
+        Product result = this._product;
+
+        this.Reset();
+
+        return result;
+    }
+}
---
+++ Builder/Interfaces/IBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace Builder.Interfaces
+{
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+}
