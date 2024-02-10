779a94aaa3972c4f78cdc0767c7199aa782385a5
Torosyan David <davit.torosyan.0214@gmail.com>
2024-02-10 17:09:23 +0400

    Adapter

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,17 @@
+﻿namespace Adapter
+{
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
